bcb1a50bf32f3310c6ef4d69fe863dae730335f9
araujo <theriancirculo@protonmail.com>
2025-12-03 17:50:01 -0300

    feat: add .gitignore, implement SQLite initialization and error handling

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"errors"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -12,9 +12,19 @@ var (
 )
 
 func Init() error {
+	var err error
 
-	return errors.New("fake err")
+	db, err = InitializeSQLite()
 
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite %v", err)
+	}
+
+	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {

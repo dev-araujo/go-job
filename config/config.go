af05cb867fdd48d154c225fb32178ffeaf78ecf9
araujo <theriancirculo@protonmail.com>
2025-12-03 11:30:42 -0300

    feat: add configuration and logging setup, and define job opening schema

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+
+	return errors.New("fake err")
+
+}
+
+func GetLogger(p string) *Logger {
+
+	logger = NewLogger(p)
+
+	return logger
+
+}
--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/dev-araujo/go-job/config"
 	r "github.com/dev-araujo/go-job/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialized error: %v", err)
+		return
+
+	}
 
 	r.Initialize()
 
